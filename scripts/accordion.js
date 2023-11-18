7b48b720a01bd6fae5bd37bdafc5faf971e7aeb8
Emile0001 <boteseh@gmail.com>
2023-11-18 20:33:07 +0200

    Bug

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -4,15 +4,8 @@ let accordion__panels = document.getElementsByClassName("accordion__panel");
 // Loop through each accordion panel and add an event listener
 for (let i = 0; i < accordion__panels.length; i++) {
     accordion__panels[i].addEventListener("click", function () {
-        // Find the associated panel__wrapper element for the clicked accordion__panel
-        let panel__wrapper = this.nextElementSibling;
-        // Toggle the class name when the panel is clicked
-        if (panel__wrapper.classList.contains("panel__wrapper--open")) {
-            panel__wrapper.classList.remove("panel__wrapper--open");
-            panel__wrapper.classList.add("panel__wrapper--closed");
-        } else {
-            panel__wrapper.classList.remove("panel__wrapper--closed");
-            panel__wrapper.classList.add("panel__wrapper--open");
-        }
+        document
+            .getElementsByClassName("panel__wrapper--closed")
+            .classList.toggle("panel__wrapper--closed");
     });
 }
